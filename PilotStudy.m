f7fb5c281a6fcfbc721688dcf3563ba319976c07
maxwellre <yitianshao@gmail.com>
2018-12-21 16:37:41 -0800

    Final adjustment before the official pilot study.

--- PilotStudy.m
+++ PilotStudy.m
@@ -5,7 +5,7 @@ close all
 clearvars
 % -------------------------------------------------------------------------
 % Global variable
-global isStarting isChoosing currChoice outQueue sNI text_h2 expData subjectID
+global isStarting isChoosing isPlayed currChoice outQueue sNI text_h2 expData subjectID
 % -------------------------------------------------------------------------
 % Experiment Configuration 
 figSize = [20,100,1880,800];
@@ -61,6 +61,7 @@ pauseSig = zeros(1,round(pauseTimeInSec*Fs));
 winLenArray = round(winFact.*(Fs./freqArray));  
 
 sigSeg = cell(numSigs,2); % Segment of individual signals
+sigCenterFreq = NaN(numSigs,1);
 for i = 1:numSigs
     winLen = winLenArray(i); % Windowing a sin with certain lengths
     
@@ -70,10 +71,15 @@ for i = 1:numSigs
     
     temp = AmpScale * ampProfile(i)*carrier.*winSig; 
     
-    temp = highpass(temp,hpFreq,Fs); % High-pass filtering
+    temp = highpass(temp,hpFreq,Fs); % High-pass filtering 
+    temp = lowpass(temp,1000,Fs,'Steepness',0.5); % Low-pass filtering
     
     sigSeg{i,1} = temp;
     sigSeg{i,2} = sprintf('%.0fHz',freqArray(i));
+    
+    % Compute spectral centroid of each signal
+    [sp, f] = spectr(sigSeg{i,1}, Fs, [0 1000]);
+    sigCenterFreq(i) = sum(sp.*f')./sum(sp);
 end
 
 % Signal A: Concentrating to the tip of index finger
@@ -93,7 +99,6 @@ wnA = [pauseSig, wnA, pauseSig];
 wnA = highpass(wnA,hpFreq,Fs); % High-pass filtering
 wnA = 2*lowpass(wnA,1000,Fs,'Steepness',0.5); % Low-pass filtering
 
-
 % Signal B: Spreading to the whole hand.
 sigB = [];
 wnB = [];
@@ -110,6 +115,18 @@ sigB = [pauseSig, sigB, pauseSig];
 wnB = [pauseSig, wnB, pauseSig];
 wnB = highpass(wnB,hpFreq,Fs); % High-pass filtering
 wnB = 2*lowpass(wnB,1000,Fs,'Steepness',0.5); % Low-pass filtering
+
+% Signal reordered sequence
+%rs_ind = [4, 8, 1, 7, 3, 6, 10, 2, 9, 5];
+%rs_ind = [4, 8, 1, 7, 3, 10, 6, 9, 2, 5];
+%rs_ind = [5, 8, 1, 7, 4, 10, 3, 9, 2, 6];
+rs_ind = [5, 8, 2, 9, 4, 6, 3, 10, 1, 7];
+% this balances spec in 1st, 2nd half and df considerations
+rs = [];
+for i = 1:numSigs    
+    rs = [rs, sigSeg{rs_ind(i),1}, zeroSig];
+end
+rs = [pauseSig, rs, pauseSig];
 
 %% initialize the NI terminal
 outQueue = [];
@@ -155,13 +172,16 @@ else
 end
 
 %% Looping trials 
+isPlayed = 0;
 
 % Randomization -----------------------------------------------------------
-% 1 = SigA, 2 = SigB, 3 = wnA, 4 = wnB, 5 = rsA, 6 = rsB
-% trialOrder = [ones(1,TrialNum),2*ones(1,TrialNum),3*ones(1,TrialNum),...
-%     4*ones(1,TrialNum),5*ones(1,TrialNum),6*ones(1,TrialNum)];
-trialOrder = [ones(1,TrialNum),2*ones(1,TrialNum),3*ones(1,TrialNum),4*ones(1,TrialNum)];
-% trialOrder = [3*ones(1,TrialNum), 4*ones(1,TrialNum)];
+% % % 1 = SigA, 2 = SigB, 3 = wnA, 4 = wnB, 5 = rsA, 6 = rsB
+% % % trialOrder = [ones(1,TrialNum),2*ones(1,TrialNum),3*ones(1,TrialNum),...
+% % %     4*ones(1,TrialNum),5*ones(1,TrialNum),6*ones(1,TrialNum)];
+
+% 1 = SigA, 2 = SigB, 3 = wnA, 4 = wnB, 5 = rs (reordered sequence)
+trialOrder = [ones(1,TrialNum),2*ones(1,TrialNum),3*ones(1,TrialNum),...
+    4*ones(1,TrialNum),5*ones(1,2*TrialNum)];
 
 totalTrialNum = length(trialOrder);
 trialOrder = trialOrder(randperm(totalTrialNum));
@@ -180,12 +200,12 @@ pic_left = imshow([]);
 % Left button
 bt_left = uicontrol('Style', 'pushbutton', 'String', choiceStr{1},...
     'FontSize',20,...
-    'Position', [550 120 200 80], 'BackgroundColor',[0.95,0.95,0.95],...
+    'Position', [550 220 200 80], 'BackgroundColor',[0.95,0.95,0.95],...
     'Callback', @chooseLeft, 'BusyAction','cancel');
 
 % Textbox for message
 text_h = uicontrol('Style','text','BackgroundColor','w',...
-    'Position',[760 700 300 50], 'String',[],'FontSize',24);
+    'Position',[740 700 300 50], 'String',[],'FontSize',24);
 text_h2 = uicontrol('Style','text','BackgroundColor','w',...
     'Position',[20 660 400 30], 'String',[],'FontSize',18);
 
@@ -196,19 +216,19 @@ pic_right = imshow([]);
 % Right button
 bt_right = uicontrol('Style', 'pushbutton', 'String', choiceStr{2},...
     'FontSize',20,...
-    'Position', [950 120 200 80], 'BackgroundColor',[0.95,0.95,0.95],...
+    'Position', [990 220 200 80], 'BackgroundColor',[0.95,0.95,0.95],...
     'Callback', @chooseRight, 'BusyAction','cancel');
 
 % Play signal button
 bt_play = uicontrol('Style', 'pushbutton', 'String', 'Play',...
     'FontSize',20,...
-    'Position', [770 220 160 80], 'BackgroundColor',[0.98,0.98,0.98],...
+    'Position', [790 220 160 80], 'BackgroundColor',[0.98,0.98,0.98],...
     'Callback', @playSignal, 'BusyAction','cancel');
 
 % Submit button
 bt_submit = uicontrol('Style', 'pushbutton', 'String', 'Submit',...
     'FontSize',20,...
-    'Position', [770 20 160 80], 'BackgroundColor',[1,1,1],...
+    'Position', [790 100 160 80], 'BackgroundColor',[1,1,1],...
     'Callback', @submitAnswer, 'BusyAction','cancel');
 
 % Figure close button (end the program)
@@ -222,6 +242,8 @@ expData = table('Size',[totalTrialNum columnNum],'VariableTypes',varTypes,...
     'VariableNames',varNames);
 
 for i = 1:totalTrialNum    
+    isPlayed = 0;
+    
     text_h.String = sprintf('Trial %d',i);
     
     choiceInd = randperm(2); % Randomize the left and right options
@@ -246,33 +268,34 @@ for i = 1:totalTrialNum
             outQueue = wnA;
         case 4 % wnB (White noise with same length as sigB)
             outQueue = wnB;
-        case 5 % Random Sequence
+        case 5 % Reordered Sequence
+            outQueue = rs;
             
-%         case 5 % rsA (Random Sequence with both ends same as sigA)
-%             rs = [];
-%             rand_ind = (1+randperm(numSigs-2));
-%             for j = rand_ind
-%                 rs = [rs, sigSeg{j,1}, zeroSig];
-%             end
-%             outQueue = [pauseSig, sigSeg{1,1}, zeroSig,...
-%                 rs, sigSeg{numSigs,1}, zeroSig, pauseSig];
-%             expData.Additional(i) = {[1,rand_ind,numSigs]};
-%         case 6 % rsB (Random Sequence with both ends same as sigB)
-%             rs = [];
-%             rand_ind = (1+randperm(numSigs-2));
-%             for j = rand_ind
-%                 rs = [rs, sigSeg{j,1}, zeroSig];
-%             end
-%             outQueue = [pauseSig, sigSeg{numSigs,1}, zeroSig,...
-%                 rs, sigSeg{1,1}, zeroSig, pauseSig];
-%             expData.Additional(i) = {[numSigs,rand_ind,1]};
+% % %         case 5 % rsA (Random Sequence with both ends same as sigA)
+% % %             rs = [];
+% % %             rand_ind = (1+randperm(numSigs-2));
+% % %             for j = rand_ind
+% % %                 rs = [rs, sigSeg{j,1}, zeroSig];
+% % %             end
+% % %             outQueue = [pauseSig, sigSeg{1,1}, zeroSig,...
+% % %                 rs, sigSeg{numSigs,1}, zeroSig, pauseSig];
+% % %             expData.Additional(i) = {[1,rand_ind,numSigs]};
+% % %         case 6 % rsB (Random Sequence with both ends same as sigB)
+% % %             rs = [];
+% % %             rand_ind = (1+randperm(numSigs-2));
+% % %             for j = rand_ind
+% % %                 rs = [rs, sigSeg{j,1}, zeroSig];
+% % %             end
+% % %             outQueue = [pauseSig, sigSeg{numSigs,1}, zeroSig,...
+% % %                 rs, sigSeg{1,1}, zeroSig, pauseSig];
+% % %             expData.Additional(i) = {[numSigs,rand_ind,1]};
         otherwise
             error('Unidentified Trial')          
     end
     text_h2.String = 'Playing the signal ...';
     bt_play.BackgroundColor = [0.5,1,0.4];
-    queueOutputData(sNI,outQueue');
-    sNI.startForeground;    
+% % %     queueOutputData(sNI,outQueue'); % Remove the auto play feature
+% % %     sNI.startForeground;    
     while sNI.IsLogging && isvalid(fig_h)      
         bt_right.BackgroundColor = [1,1,1];
         bt_left.BackgroundColor = [1,1,1];
@@ -296,11 +319,11 @@ for i = 1:totalTrialNum
             bt_play.BackgroundColor = [0.98,0.98,0.98];
             
             if currChoice == 1 % Left button pushed
-                text_h2.String = sprintf('Your choice: %s',choiceStr{1});
+                text_h2.String = sprintf('Your choice: %s',bt_left.String);
                 bt_right.BackgroundColor = [0.98,0.98,0.98];
                 bt_left.BackgroundColor = [1,0.5,0.5];
             elseif currChoice == 2 % Right button pushed
-                text_h2.String = sprintf('Your choice: %s',choiceStr{2});
+                text_h2.String = sprintf('Your choice: %s',bt_right.String);
                 bt_left.BackgroundColor = [0.98,0.98,0.98];
                 bt_right.BackgroundColor = [1,0.5,0.5];
             else
@@ -351,32 +374,41 @@ function startExp(~, ~, etf)
 end
 
 function chooseLeft(hObject, ~) 
-    global currChoice sNI;
+    global currChoice sNI isPlayed text_h2;
     if ~sNI.IsLogging
-        button_state = get(hObject,'Value');
-        if button_state == get(hObject,'Max')
-            currChoice = 1; % Left button pushed
+        if isPlayed
+            button_state = get(hObject,'Value');
+            if button_state == get(hObject,'Max')
+                currChoice = 1; % Left button pushed
+            end
+        else
+            text_h2.String = 'Play the signal first!'; pause(0.8);
         end
     end
 end
 
 function chooseRight(hObject, ~) 
-    global currChoice sNI;
+    global currChoice sNI isPlayed text_h2;
     if ~sNI.IsLogging
-        button_state = get(hObject,'Value');
-        if button_state == get(hObject,'Max')
-            currChoice = 2; % Right button pushed
+        if isPlayed
+            button_state = get(hObject,'Value');
+            if button_state == get(hObject,'Max')
+                currChoice = 2; % Right button pushed
+            end
+        else
+            text_h2.String = 'Play the signal first!'; pause(0.8);
         end
     end
 end
 
 function playSignal(hObject, ~)
-    global outQueue sNI text_h2;
+    global outQueue sNI text_h2 isPlayed;
     if ~sNI.IsLogging
         text_h2.String = 'Playing the signal ...';
         hObject.BackgroundColor = [0.5,1,0.4];
         queueOutputData(sNI,outQueue');
         sNI.startForeground; 
+        isPlayed = 1;
     end
 end
 
@@ -388,8 +420,7 @@ function submitAnswer(~, ~)
     if currChoice > 0
         isChoosing = 0;
     else
-        text_h2.String = 'Make your choice first!';
-        pause(0.8);
+        text_h2.String = 'Make your choice first!'; pause(0.8);
     end
 end
 
--- PilotStudyAnalysis.m
+++ PilotStudyAnalysis.m
@@ -17,20 +17,23 @@ rate1 = sum(expData.SubmittedAnswer == expData.StimulusType)/...
 rate2 = sum(expData.SubmittedAnswer == (expData.StimulusType-2))/...
     (2*oneTypeTrialNumber); % Compare answer with stimulus type 3 and 4
 
-rate3 = sum(expData.SubmittedAnswer == (expData.StimulusType-4))/...
-    (2*oneTypeTrialNumber); % Compare answer with stimulus type 5 and 6
+% rate3 = sum(expData.SubmittedAnswer == (expData.StimulusType-4))/...
+%     (2*oneTypeTrialNumber); % Compare answer with stimulus type 5 and 6
+Z = expData.SubmittedAnswer(expData.StimulusType == 5);
+rate3 = sum(Z == 1)/length(Z);
 
 fprintf('sigA-sigB pair identification rate = %.1f %%\n',100*rate1);
 fprintf('wnA-wnB pair identification rate = %.1f %%\n',100*rate2);
-fprintf('rsA-rsB pair identification rate = %.1f %%\n',100*rate3);
+% fprintf('rsA-rsB pair identification rate = %.1f %%\n',100*rate3);
+fprintf('Rate of rs identified as A = %.1f %%\n',100*rate3);
 
 % -------------------------------------------------------------------------
 StimulusLabel = {'Concentrating (A)','Spreading (B)','White Noise (A)',...
-    'White Noise (B)','Random Sequence (A)','Random Sequence (B)'};
+    'White Noise (B)','Reordered Sequence'};
 figure('Position',[50,150,1200,600]);
 histEdges = linspace(min(expData.ResponseTime),max(expData.ResponseTime),...
     50);
-for i = 1:6
+for i = 1:5
     subplot(3,2,i)
     resTime = expData.ResponseTime(expData.StimulusType == i);
     histogram(resTime,histEdges,'EdgeColor','none');
@@ -38,4 +41,17 @@ for i = 1:6
     title(StimulusLabel{i})
     box off
 end
+% StimulusLabel = {'Concentrating (A)','Spreading (B)','White Noise (A)',...
+%     'White Noise (B)','Random Sequence (A)','Random Sequence (B)'};
+% figure('Position',[50,150,1200,600]);
+% histEdges = linspace(min(expData.ResponseTime),max(expData.ResponseTime),...
+%     50);
+% for i = 1:6
+%     subplot(3,2,i)
+%     resTime = expData.ResponseTime(expData.StimulusType == i);
+%     histogram(resTime,histEdges,'EdgeColor','none');
+%     xlabel('Response Time (secs)')
+%     title(StimulusLabel{i})
+%     box off
+% end
 
